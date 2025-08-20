cfdfbf850e482411613ec5600df9066f82632bfd
Daniel Mader <daniel.mader@impierce.com>
2025-08-21 01:12:04 +0200

    test: error mapping

--- src/error.rs
+++ src/error.rs
@@ -8,14 +8,6 @@ pub enum MongoAggregateError {
     UnknownError(Box<dyn std::error::Error + Send + Sync + 'static>),
 }
 
-// impl std::error::Error for MongoAggregateError {}
-
-impl From<serde_json::Error> for MongoAggregateError {
-    fn from(error: serde_json::Error) -> Self {
-        MongoAggregateError::UnknownError(Box::new(error))
-    }
-}
-
 impl From<mongodb::error::Error> for MongoAggregateError {
     fn from(error: mongodb::error::Error) -> Self {
         match error.kind.as_ref() {
@@ -39,10 +31,15 @@ impl From<mongodb::error::Error> for MongoAggregateError {
     }
 }
 
+impl From<mongodb::bson::de::Error> for MongoAggregateError {
+    fn from(error: mongodb::bson::de::Error) -> Self {
+        MongoAggregateError::DeserializationError(Box::new(error))
+    }
+}
+
 impl From<mongodb::bson::document::ValueAccessError> for MongoAggregateError {
     fn from(error: mongodb::bson::document::ValueAccessError) -> Self {
-        // TODO: is this a deserialization error?
-        MongoAggregateError::UnknownError(Box::new(error))
+        MongoAggregateError::DeserializationError(Box::new(error))
     }
 }
 
@@ -56,5 +53,32 @@ impl From<MongoAggregateError> for PersistenceError {
             }
             MongoAggregateError::UnknownError(err) => PersistenceError::UnknownError(err),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_maps_bson_deserialization_error() {
+        let bson_error =
+            mongodb::bson::from_bson::<u32>(mongodb::bson::bson!({ "foo": "bar" })).unwrap_err();
+        let mongo_error: MongoAggregateError = bson_error.into();
+        assert!(matches!(
+            mongo_error,
+            MongoAggregateError::DeserializationError(_)
+        ));
+    }
+
+    #[test]
+    fn test_maps_bson_value_access_error() {
+        let doc = mongodb::bson::doc! { "foo": "bar" };
+        let access_error = doc.get_i32("foo").unwrap_err();
+        let mongo_error: MongoAggregateError = access_error.into();
+        assert!(matches!(
+            mongo_error,
+            MongoAggregateError::DeserializationError(_)
+        ));
     }
 }
