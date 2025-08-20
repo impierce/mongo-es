1835900f402ebb67887019058a99137a8a0da1bd
Daniel Mader <daniel.mader@impierce.com>
2025-08-20 10:58:51 +0200

    feat: re-export `mongodb::Client`

--- src/types.rs
+++ src/types.rs
@@ -2,4 +2,6 @@ use cqrs_es::{persist::PersistedEventStore, CqrsFramework};
 
 use crate::MongoEventRepository;
 
+pub use mongodb::Client;
+
 pub type MongoCqrs<A> = CqrsFramework<A, PersistedEventStore<MongoEventRepository, A>>;
